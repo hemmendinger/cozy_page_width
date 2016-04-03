fdd4c53512ace0a46ecc1813de9d87ffb73c1724
Hemmendinger <github@hemmendinger.net>
2016-04-03 18:03:39 -0400

    content.js

---
+++ content.js
@@ -0,0 +1,18 @@
+function setMarginPercent(leftMarginInt, rightMarginInt) {
+    document.body.style.leftMargin = leftMarginInt + "%";
+    document.body.style.marginRight = rightMarginInt + "%";
+}
+
+
+// Icon click listener: adds 25% margins
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+        if (request.backgroundClick == true) {
+            setMarginPercent(25, 25);
+            sendResponse(
+				{result: "content.js processed backgroundClick == true"});
+        }
+});
+
+
+
