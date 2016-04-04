7a182a78279fe736e21faf602bac9a3f2c3cfcf9
Hemmendinger <github@hemmendinger.net>
2016-04-04 17:16:04 -0400

    making margins set by extension togglable, badge doesnt update on toggle yet

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
+// State of margins for toggling changes
+var cozy_settings = {
+	origMarginLeft: "",
+	origMarginRight: "",
+	margins_changed: false,
+};
+
+
+
 function setMarginPercent(leftMarginInt, rightMarginInt) {
     document.body.style.marginLeft = leftMarginInt + "%";
     document.body.style.marginRight = rightMarginInt + "%";
@@ -8,7 +17,20 @@ function setMarginPercent(leftMarginInt, rightMarginInt) {
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
         if (request.backgroundClick == true) {
-            setMarginPercent(25, 25);
+			
+			if (cozy_settings.margins_changed == false) {
+				// If not changed, then set left and rightmargins to 25%
+				cozy_settings.origMarginLeft = (document.body.style !== null) ? document.body.style.marginLeft : "";
+				cozy_settings.origMarginRight = (document.body.style !== null) ? document.body.style.marginRight : "";
+				setMarginPercent(25, 25);
+				cozy_settings.margins_changed = true;
+			} else {
+				// If changed, then set the margins back to their original settings
+				document.body.style.marginLeft = cozy_settings.origMarginLeft;
+				document.body.style.marginRight = cozy_settings.origMarginRight;
+				cozy_settings.margins_changed = false;
+			}
+
             sendResponse(
 				{result: "content.js processed backgroundClick == true"}
 			);
